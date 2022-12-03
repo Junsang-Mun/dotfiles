bcd71c980244a2e8917a1c04ecde3e52e5b24424
Junsang <mun.js@aol.com>
2022-12-03 17:32:29 +0900

    Update installVimrc.sh

--- installVimrc.sh
+++ installVimrc.sh
@@ -1,14 +1,26 @@
+#!/bin/bash
+
 RCFILE=~/.vimrc
-echo ".vimrc 파일이 존재하는지 확인합니다."
-if test -f "$RCFILE";then
-	echo ".vimrc 파일이 존재합니다. 백업할까요?"
-	read -p "[Y/n] : " response
-	case $response in
-		[Nn]* ) rm ~/.vimrc; break;;
-		* ) mv ~/.vimrc ~/.vimrc.old && echo "백업이 완료되었습니다. ~/.vimrc.old 파일을 확인하세요.";;
-	esac
+INSTALL=false
+
+echo "이 스크립트는 사전 설정된 .vimrc 파일을 설치하는 스크립트입니다."
+echo ".vimrc를 설치할까요?"
+read -p "[y/N] : " response
+case $response in
+	[Yy]* ) INSTALL=true; break;;
+	* ) INSTALL=false;;
+esac
+if [ "$INSTALL" = true ]; then
+	if test -f "$RCFILE"; then
+		echo ".vimrc 파일이 존재합니다. ~/.vimrc.old로 백업합니다."
+		mv ~/.vimrc ~/.vimrc.old && echo "백업이 완료되었습니다."
+	fi
+	curl -s -o ~/.vimrc https://raw.githubusercontent.com/Junsang-Mun/dotfiles/main/.vimrc
+	vim -c 'PlugInstall' \
+		-c 'qa!'
+	echo '설치가 완료되었습니다.'
+	exit;
+else
+	echo '설치를 취소합니다.'
+	exit;
 fi
-curl -s -o ~/.vimrc https://raw.githubusercontent.com/Junsang-Mun/dotfiles/main/.vimrc
-vim -c 'PlugInstall' \
-	-c 'qa!'
-echo '설치가 완료되었습니다.'
